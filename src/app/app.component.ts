16baf6167dc9913ca37fbd3f96881ca1827ca054
jelizalde <elizald.jd@gmail.com>
2022-03-20 14:52:35 -0600

    se agrega materilize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'servicenow';
+export class AppComponent implements OnInit, AfterViewInit {
+  options = { fullWidth: false };
+  items = ["https://picsum.photos/200/300?image=0", "https://picsum.photos/200/300?image=1", "https://picsum.photos/200/300?image=2", "https://picsum.photos/200/300?image=3", "https://picsum.photos/200/300?image=4"]
+
+  hrefs = ['one', 'two', 'three', 'four', 'five'];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    // ERROR
+    // Error: Cannot read property 'clientWidth' of undefined
+    // let elems = document.querySelectorAll('.carousel');
+    // let instances = M.Carousel.init(elems, this.options);
+  }
+
+  ngAfterViewInit() {
+    // no errors
+    let elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems, this.options);
+  }
 }
